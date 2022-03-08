212ef9251a6d0b5c6a1469952100df48bc2ef86b
Rohit Sandirane <rohit.sandirane@gmail.com>
2022-03-08 16:11:33 +0530

    hosting in heroku

--- server.js
+++ server.js
@@ -29,11 +29,10 @@ app.use('/api/products',productRoutes);
 app.use('/api/brands',brandRoutes);
 app.use('/api/stocks',stockRoutes);
 
-// Step 1:
-app.use(express.static(path.resolve(__dirname, "./client/build")));
-// Step 2:
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+app.use(express.static(path.join(__dirname,'client','build')));
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','build','index.html'));
 });
 
 
